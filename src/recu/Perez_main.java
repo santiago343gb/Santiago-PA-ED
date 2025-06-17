71bbb56c03afc61d9ca29a921ae49a54b5a87fad
PEREZ ALBARRAN <santiago.perez2@isepceu.es>
2025-06-17 17:25:13 +0200

    Modificacion

--- src/recu/Perez_main.java
+++ src/recu/Perez_main.java
@@ -7,27 +7,34 @@ public class Perez_main {
         Perez_circulo circulo1 = new Perez_circulo(1, "Circulo1", 5.0, "Rojo");
         Perez_circulo circulo2 = new Perez_circulo(2, "Circulo2", 3.0, "Azul");
 
-        
         Perez_rectangulo rectangulo1 = new Perez_rectangulo(1, 4.0, 6.0, "Verde");
-        Perez_rectangulo rectangulo2 = new Perez_rectangulo(2, 2.0, 2.0, "Amarillo"); // cuadrado
+        Perez_rectangulo rectangulo2 = new Perez_rectangulo(2, 2.0, 2.0, "Amarillo"); 
 
-       
         double areaCirculo1 = Math.PI * circulo1.getRadio() * circulo1.getRadio();
         double areaCirculo2 = Math.PI * circulo2.getRadio() * circulo2.getRadio();
 
         double areaRectangulo1 = rectangulo1.getLadoA() * rectangulo1.getLadoB();
         double areaRectangulo2 = rectangulo2.getLadoA() * rectangulo2.getLadoB();
 
-        
         System.out.println("Área del " + circulo1.getNombre() + ": " + areaCirculo1);
         System.out.println("Área del " + circulo2.getNombre() + ": " + areaCirculo2);
         System.out.println("Área del Rectángulo 1: " + areaRectangulo1);
         System.out.println("Área del Rectángulo 2: " + areaRectangulo2);
 
-     
         double diferenciaAreas = Math.abs(areaCirculo1 - areaRectangulo2);
         System.out.println("Diferencia de áreas entre " + circulo1.getNombre() + " y el cuadrado: " + diferenciaAreas);
+
+        
+        System.out.println("Perímetro de " + circulo1.getNombre() + ": " + calcularPerimetroCirculo(circulo1));
+        System.out.println("Perímetro del Rectángulo 1: " + calcularPerimetroRectangulo(rectangulo1));
     }
-//doma
-  
+
+    private static double calcularPerimetroCirculo(Perez_circulo c) {
+        return 2 * Math.PI * c.getRadio();
+    }
+//git cambio
+    private static double calcularPerimetroRectangulo(Perez_rectangulo r) {
+        return (2 * r.getLadoA()) + (2 * r.getLadoB());
+    }
 }
+//
